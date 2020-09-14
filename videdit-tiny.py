8bf281daaccda322a770b5023a90c825eedbad66
Henning Hollermann <hollermann@physik.rwth-aachen.de>
2020-09-14 22:49:07 +0200

    fix missing images in dir

--- videdit-tiny.py
+++ videdit-tiny.py
@@ -9,8 +9,9 @@ import datetime
 ffmpeg_bin = "/usr/bin/ffmpeg"
 ffprobe_bin = "/usr/bin/ffprobe"
 exiftool_bin = "/usr/bin/exiftool"
-ffmpeg_default_opts = ["-c:a", "aac", "-crf", "19"]
-ffmpeg_quick_opts = ["-c:a", "aac", "-crf", "30"]
+ffmpeg_quiet = ["-hide_banner", "-loglevel", "panic"]
+ffmpeg_default_opts = ffmpeg_quiet+["-c:a", "aac", "-crf", "19"]
+ffmpeg_quick_opts = ffmpeg_quiet+["-c:a", "aac", "-b:a", "96k", "-movflags", "+faststart", "-crf", "30"]
 timeshift = "02:00"
 
 __author__ = "Henning Hollermann"
@@ -27,14 +28,11 @@ parser.add_argument("--quick", default=False, action='store_true',
                         help="Quick mode: copy audio, use fast compression")
 parser.add_argument("--dry", default=False, action='store_true', 
                         help="Do not execute commands, print only.")
-parser.add_argument("--overwrite", default=False, action='store_true', 
-                        help="Overwrite existing files.")
-parser.add_argument("--rename", default=False, action='store_true', 
-                        help="Rename output video based on image file from given imagedir "
-                        "with the closest timestamp")
-parser.add_argument("--imagedir", default="./", 
-                        help="Directory with image files to guess the file name "
-                        "for the video from the exif metadata.")
+parser.add_argument("--no_suggest", default=False, action='store_true',
+                        help="Do not suggest filenames based on exif-data of related "
+                             "image files.")
+parser.add_argument("-f","--force", default=False, action='store_true', 
+                        help="Force. Overwrite existing files.")
 parser.add_argument("-v", "--version", action='store_true', help="Print version")
 
 actionsgroup = parser.add_argument_group('ACTIONS')
@@ -48,15 +46,26 @@ actionsgroup.add_argument("--transpose", choices=["CCWFlip", "90", "-90", "180",
 actionsgroup.add_argument("--mp4", action="store_true", 
                             help="Only encode input video files as mp4. This overrides " 
                             "cut, scale, fade and transpose.")
+actionsgroup.add_argument("--rename", default=False, action='store_true', 
+                        help="Rename output video based on image file from given imagedir "
+                        "with the closest timestamp")
+
+renamegroup = parser.add_argument_group('Arguments to --rename')
+renamegroup.add_argument("-d","--dir", default="./", 
+                        help="Directory with image files to guess the file name "
+                        "for the video from the exif metadata.")
+renamegroup.add_argument("-t","--tolerance", default="300", 
+                        help="Time window in which an image file is treated as related "
+                        "to the given video file.")
 
 scalegroup = parser.add_argument_group('Arguments to --scale')
 scalegroup.add_argument("-s", action='append', choices=["66","50","33", "720", "540", "320"], 
                             help="Add output video resolution. Default: 66%% (720p for HD input video)")
 
 fadegroup = parser.add_argument_group('Arguments to --fade')
-fadegroup.add_argument("-fadetime", default="0.3", 
+fadegroup.add_argument("-tf","--fadetime", default="0.3", 
                             help="Time to fadein and fadeout video. Default: 0.3 sec")
-fadegroup.add_argument("-fadeblack", default="0.1", 
+fadegroup.add_argument("-tb","--fadeblack", default="0.1", 
                             help="Time to delay fade, creating black time at "
                             "the beginning and the end of the video. Default: 0.1 sec")
 
@@ -75,11 +84,16 @@ def convert(source,target,arguments):
         ffmpeg_arguments = arguments + ffmpeg_quick_opts
     else:
         ffmpeg_arguments = arguments + ffmpeg_default_opts
-        
+
     if args.dry:
-        print(ffmpeg_bin,'-i', source, *arguments, target)
+        print(ffmpeg_bin,'-i', source, *ffmpeg_arguments, target)
     else:
-        subprocess.check_call([ffmpeg_bin,'-i', source, *arguments, target])
+        print("Converting {} -> {}".format(source,target))
+        try:
+            subprocess.check_call([ffmpeg_bin,'-i', source, *ffmpeg_arguments, target])
+        except subprocess.CalledProcessError:
+            print("Aborted.")
+            return
         copy_timstamps(source,target)
 
 
@@ -91,16 +105,18 @@ def copy_timstamps(sourcefile,targetfile):
     exclude_tags=["--ImageSize", "--ImageWidth","--ImageHeight"]
     subprocess.check_call([exiftool_bin, '-ee', '-overwrite_original', '-tagsFromFile', 
                 sourcefile, *exclude_tags, targetfile])
-    subprocess.check_call([exiftool_bin, '-overwrite_original', '-DateTimeOriginal<${datetimeoriginal}+'+timeshift,
+    subprocess.check_call([exiftool_bin, '-overwrite_original', '-DateTimeOriginal<${datetimeoriginal}+' + timeshift,
                 targetfile])
-    timestamp = subprocess.check_output(exiftool_bin + " -DateTimeOriginal -S -s -d \'%Y-%m-%d %H:%M:%S%z\' " + sourcefile,
+    timestamp = subprocess.check_output("{0} -DateTimeOriginal -S -s "
+                                        "-d \'%Y-%m-%d %H:%M:%S%z\' {1}".format(exiftool_bin, sourcefile),
                   shell=True)
     subprocess.check_call(['touch', '-d', timestamp, targetfile])
 
 
 def get_small_edge(file):
     """Use ffprobe to determine the length video's short edge"""
-    vidsize = subprocess.check_output(ffprobe_bin + " -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 " + file, 
+    vidsize = subprocess.check_output("{0} -v error -select_streams v:0 "
+                                      "-show_entries stream=width,height -of csv=s=x:p=0 {1}".format(ffprobe_bin, file),
                 shell=True)
     size = vidsize.decode("utf-8").split("\n")[0].split("x")
     l = [int(i) for i in size]
@@ -108,13 +124,14 @@ def get_small_edge(file):
 
 def get_duration(file):
     """Use ffprobe to determine the duration of a video file"""
-    duration = subprocess.check_output(ffprobe_bin + " -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 " + file,
-                shell=True)
+    duration = subprocess.check_output("{0} -v error -show_entries format=duration "
+                                       "-of default=noprint_wrappers=1:nokey=1 {1}".format(ffprobe_bin, file),
+                                       shell=True)
     return duration
 
 def get_timestr(seconds):
-    """Construct hh:mm:ss string from seconds."""
-    return str(datetime.timedelta(seconds=float(seconds)))
+    """Construct dd days, hh:mm:ss string from seconds."""
+    return str(datetime.timedelta(seconds=seconds)).rstrip("0")
 
 def get_sec(time_str):
     """Get seconds from time string hh:mm:ss"""
@@ -131,27 +148,49 @@ def check_inputfile_exists():
     return all_exist
 
 def get_closest_filepath(file,tolerance=300):
-    """Uses exiftool to find the file with the closest exif timestamp compared to the input video file"""
-    vid_timestamp = int(subprocess.check_output(exiftool_bin + " -datetimeoriginal -S -s -d %s -T " + file, 
-                    shell=True).decode("utf-8").split("\n")[0])
-    image_table = subprocess.check_output(exiftool_bin + " -filepath -datetimeoriginal " 
-                    "-S -s -r -d %s -T -if \'$mimetype =~ m\"^image/[^x]\"\' " + args.imagedir, 
-                    shell=True).decode("utf-8").split("\n")
-    files = [item.split("\t")[0] for item in image_table if item]
+    """
+    Uses exiftool to find the file with the closest exif timestamp compared to the input video file
+    """
+    vid_timestamp_str = subprocess.check_output("{0} -datetimeoriginal -FileModifyDate "
+                                                "-S -s -d %s -T {1}".format(exiftool_bin, file),
+                    shell=True).decode("utf-8").split("\n")[0].split("\t")
+    try:
+        vid_timestamp = int(vid_timestamp_str[0])
+    except ValueError:
+        try:
+            vid_timestamp = int(vid_timestamp_str[1])
+            print("Info: using file modification time as indicator for " 
+            "file {} due to a nonexistent DateTimeOriginal exif field.".format(file))
+        except ValueError:
+            print("Error: neither DateTimeOriginal nor FileModifyDate was found for file {}. Exiting".format(file))
+            sys.exit()
+    try:
+        exif_data_table = subprocess.check_output("{0} -filepath -datetimeoriginal -FileModifyDate " 
+                        "-S -s -r -d %s -T -if \'$mimetype =~ m\"^image/[^x]\"\' {1}".format(exiftool_bin, args.dir),
+                        shell=True).decode("utf-8").split("\n")
+    except subprocess.CalledProcessError:
+        print("Warning: no images found in directory {0}. Disabling filename suggestion.".format(args.dir))
+        args.no_suggest = True
+        return None
+    image_table = [item.split("\t") for item in exif_data_table if item]
+    files = []
     times = []
     for item in image_table:
-        if item:
-            try:
-                times.append(int(item.split("\t")[1]))
-            except:
-                times.append(0)
+        try:
+            times.append(int(item[1]))
+        except:
+            print("Warning: {} skipped.".format(file))
+            continue
+        files.append(item[0])
     time_diff = [abs(time-vid_timestamp) for time in times]
     min_time_diff = min(time_diff)
     image_closest_time = time_diff.index(min_time_diff)
     if min_time_diff > tolerance:
-        print("Warning: No related file found for file {} using the one with the closest "
-        "timestamp ({} seconds):\n{}".format(file,min_time_diff,files[image_closest_time]))
-    return files[image_closest_time]
+        print("Warning: No related file found for file {}. The one with the closest timestamp "
+        "(difference {}) was {}".format(file,get_timestr(min_time_diff),files[image_closest_time]))
+        return None
+    else:
+        return files[image_closest_time]
 
 
 def scale():
@@ -187,26 +226,28 @@ def cut():
     """Return cut video arguments"""
     if not ':' in args.ss:
         args.ss = get_timestr(float(args.ss))
-    if not args.to:
-        arg.to="end"
     elif not ':' in args.to:
         args.to = get_timestr(float(args.to))
+    if not args.to:
+        arg.to="end"
     return ["-ss", args.ss, "-to", args.to]
 
 
 def fade(file):
     """Construct fade filter filter:v and filter:a"""
-    d_in = d_out = round(float(args.fadetime),2)
-    black = round(float(args.fadeblack),2)
+    d_in = d_out = round(float(args.fadetime), 2)
+    black = round(float(args.fadeblack), 2)
     if args.cut:
-        st_in=black+get_sec(args.ss)
-        st_out=round(get_sec(args.to)-d_out-black,2)
+        st_in=black + get_sec(args.ss)
+        st_out=round(get_sec(args.to) - d_out - black, 2)
     else:
         duration = get_duration(file)
         st_in=0
-        st_out=round(float(duration)-d_out,2)
-    fade_filter_a = "afade=in:st={0}:d={1},afade=out:st={2}:d={3}".format(st_in,d_in,st_out,d_out)
-    fade_filter_v = "fade=in:st={0}:d={1},fade=out:st={2}:d={3}".format(st_in,d_in,st_out,d_out)
+        st_out=round(float(duration) - d_out, 2)
+    fade_filter_a = "afade=in:st={0}:d={1},afade=out:st={2}:d={3}".format(st_in,
+                                                                          d_in, st_out, d_out)
+    fade_filter_v = "fade=in:st={0}:d={1},fade=out:st={2}:d={3}".format(st_in,
+                                                                        d_in, st_out, d_out)
     return [fade_filter_v,fade_filter_a]
 
 
@@ -248,25 +289,32 @@ def main():
             # res_list 100 means: "do not scale at all" (returns empty filter)
             res_list = ["100"]
         for r in res_list:
-            ffmpeg_filter_v[r] = ['-filter:v',','.join([transpose_filter,scale_filter[r]]).strip(",")]
-    
+            ffmpeg_filter_v[r] = ['-filter:v', ','.join([transpose_filter, scale_filter[r]]).strip(",")]
+
     for file in args.inputfile:
         # use exiftool to get the file with the closest timestamp
-        closest_filepath = get_closest_filepath(file)
+        if not args.no_suggest:
+            suggested = get_closest_filepath(file,int(args.tolerance))
+        else:
+            suggested = None
+        if suggested and not args.no_suggest:
+            prepend = os.path.splitext(suggested)[0] + '_'
+        else:
+            prepend = ""
         # if mp4 is specified, do nothing else
         if args.mp4:
             outfile = os.path.splitext(file)[0] + outid + '.mp4'
-            suggested_outfile = os.path.splitext(closest_filepath)[0] + '_' + os.path.basename(outfile)
             if args.rename:
-                outfile = suggested_outfile
-            else:
-                print("Tip: You could rename the file manually to {} or set the --rename flag.".format(suggested_outfile))
+                outfile = prepend + os.path.basename(outfile)
+            elif suggested and not args.no_suggest:
+                print("Tip: You could rename the file manually to {0}{1} "
+                      "or set the --rename flag.".format(prepend, os.path.basename(outfile)))
             convert(file,outfile,ffmpeg_args)
         else:
             # process cut options
             # if args.to was not given, set it to the end of the file
             if args.cut and args.to == "end":
-                args.to = get_timestr(get_duration(file))
+                args.to = get_timestr(float(get_duration(file)))
                 ffmpeg_args[-1] = args.to
             if filter_v:
                 # construct the fade video and audio filters
@@ -276,17 +324,17 @@ def main():
                 # scaling filters and output file names are adjusted for every chosen resolution
                 for r in ffmpeg_filter_v.keys():
                     outfile = file + outid + "_" + r + '.mp4'
-                    suggested_outfile = os.path.splitext(closest_filepath)[0] \
-                                            + '_' + os.path.basename(outfile)
                     if args.rename:
-                        outfile = suggested_outfile
-                    else:
-                        print("Tip: You could rename the file manually to {} or set the --rename flag.".format(suggested_outfile))
+                        outfile = prepend + os.path.basename(outfile)
+                    elif suggested and not args.no_suggest:
+                        print("Tip: You could rename the file manually to {0}{1} "
+                              "or set the --rename flag.".format(prepend, os.path.basename(outfile)))
                     if args.fade:
                         ffmpeg_filter_v[r][1] = ",".join([ffmpeg_filter_v[r][1],fade_filter_v]).strip(",")
                     
                     # run ffmpeg (only prints command in DRY mode)
-                    convert(file,outfile,ffmpeg_args + ffmpeg_filter_v[r] + ffmpeg_filter_a)
+                    convert(file, outfile, ffmpeg_args + ffmpeg_filter_v[r] + ffmpeg_filter_a)
+                
 
 
 if __name__ == '__main__':
